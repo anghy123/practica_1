dd56dcb7f7e6bd2c46be62e957d22f0112d6706a
anghy123 <angela.catacora.1999@gmail.com>
2021-03-19 18:18:03 -0400

    practica_1

---
+++ practica_1/ejercicio_6/src/java/ejer6/fin.java
@@ -0,0 +1,102 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejer6;
+import java.util.Random;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author ANGELA CATACORA
+ */
+@WebServlet(name = "fin", urlPatterns = {"/fin"})
+public class fin extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        int suma,r;
+        Random r1 = new Random();
+        int valorDado1 = r1.nextInt(6)+1;
+        Random r2 = new Random();
+        int valorDado2 = r2.nextInt(6)+1;
+    response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet main</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>los dados son</h1>");
+            out.println("<h3>el dado 1 es de:"+valorDado1+" </h3>");
+            out.println("<h3>el dado 2 es de:"+valorDado2+" </h3>");
+            suma=valorDado1+valorDado1;
+            if(suma==7||suma==11){
+            out.println("<h3>ganan</h3>");
+            }   
+            else{
+            out.println("<h3>pierden</h3>");
+            }
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
