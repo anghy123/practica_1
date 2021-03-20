efa5e37088834b66c4c6bd12e312e17a3b9c8a3b
Angela Catacora <75490185+anghy123@users.noreply.github.com>
2021-03-19 21:38:38 -0400

    Update salida.java

--- practica_1/ejercicio_1/src/java/ejer1/salida.java
+++ practica_1/ejercicio_1/src/java/ejer1/salida.java
@@ -4,7 +4,8 @@
  * and open the template in the editor.
  */
 package ejer1;
-
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
@@ -63,6 +64,9 @@ public class salida extends HttpServlet {
         String nombre=request.getParameter("nombre");
         String fecha=request.getParameter("fecha");
         int fechas = Integer.parseInt(fecha);
+        Calendar fechaActual = new GregorianCalendar();
+        int fechaActual1 = fechaActual.get(Calendar.YEAR);
+        int r1=Math.abs(fechas-fechaActual1);
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
@@ -81,6 +85,7 @@ public class salida extends HttpServlet {
             out.println("<p align='center'>año de nacido:  "+fechas+"</p>");
             out.println("<br>");
             out.println("<br>");
+            out.println("<p align='center'>tu edad es de:"+r1+" años</p>");
             out.println("</body>");
             out.println("</html>");
         }
