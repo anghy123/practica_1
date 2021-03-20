5fd66455c42febc608b4e6581a621dd0a4d01f5f
anghy123 <angela.catacora.1999@gmail.com>
2021-03-19 23:52:42 -0400

    practica1_aca1

---
+++ practica1_aca1/ejercicio_1/src/java/ejer1/salida.java
@@ -0,0 +1,104 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejer1;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author ANGELA CATACORA
+ */
+@WebServlet(name = "salida", urlPatterns = {"/salida"})
+public class salida extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+       
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String nombre=request.getParameter("nombre");
+        String fecha=request.getParameter("fecha");
+        int fechas = Integer.parseInt(fecha);
+        Calendar fechaActual = new GregorianCalendar();
+        int fechaActual1 = fechaActual.get(Calendar.YEAR);
+        int r1=Math.abs(fechas-fechaActual1);
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title> datos</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1 align='center'>datos del usuario</h1>");
+            out.println("<br>");
+            out.println("<br>");
+            out.println("<p align='center'>nommbre:   "+nombre+"</p>");
+            out.println("<br>");
+            out.println("<br>");
+            out.println("<p align='center'>año de nacido:  "+fechas+"</p>");
+            out.println("<br>");
+            out.println("<br>");
+	    out.println("<p align='center'>tu edad es de:"+r1+" años</p>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
